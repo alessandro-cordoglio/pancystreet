5e55f8b88bf6ae98d0163764fba504b67ea7f17c
alessandro-cordoglio <alessandro.cord20@gmail.com>
2023-03-24 00:05:23 +0100

    add: style footer, animazioni barre

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
+
+
+/* PRIMA FILA DI BARS */
+
+
    // Seleziona l'elemento che si desidera osservare
   const target = $('.bar')[0];
 
@@ -7,15 +12,61 @@ $(document).ready(function(){
   entries.forEach(entry => {
   // Se l'elemento diventato visibile nella viewport, esegui una funzione
   if (entry.isIntersecting) {
-  const bars= $('.bar');
-  console.log(bars);
-  bars.each(function(){
-  $(this).css('height', '400px');
-  $(this).css('transform', 'translateY(0)');
-  })
+    const bars= $('.bar');
+    console.log(bars);
+    bars.each(function(){
+    $(this).css('height', '400px');
+    $(this).css('transform', 'translateY(0)');
+    })
+    }
+    });
+  });
+
+/* SECONDA FILA DI BARS */
+
+  // Seleziona gli elementi delle bar
+const bars = document.querySelectorAll('.freeart_bar');
+
+// Ottieni l'altezza della finestra di visualizzazione
+const windowHeight = window.innerHeight;
+
+// Aggiungi un listener per l'evento scroll
+window.addEventListener('scroll', () => {
+  // Ottieni la posizione della prima bar rispetto alla finestra di visualizzazione
+  const firstBarRect = bars[0].getBoundingClientRect();
+  const firstBarTop = firstBarRect.top;
+  const firstBarBottom = firstBarRect.bottom;
+
+  // Verifica se la prima bar è visibile nella viewport
+  if (firstBarTop < windowHeight && firstBarBottom > 0) {
+    // Imposta l'altezza delle altre bar in modo che siano uguali all'altezza della prima
+    const firstBarHeight = firstBarRect.height;
+    bars.forEach((bar) => {
+      bar.style.height = `${firstBarHeight}px`;
+    });
   }
-  });
-  });
+});
+
+
+// Seleziona gli elementi delle barre
+const bars3 = document.querySelectorAll('.ed_bar');
+
+// Aggiungi un listener per l'evento scroll
+window.addEventListener('scroll', () => {
+  // Ottieni le dimensioni della seconda barra rispetto alla finestra di visualizzazione
+  const secondBarRect = bars3[1].getBoundingClientRect();
+  const secondBarHeight = secondBarRect.height;
+  const secondBarTop = secondBarRect.top;
+  const secondBarBottom = secondBarRect.bottom;
+
+  // Verifica se la seconda barra è visibile nella viewport
+  if (secondBarTop < windowHeight && secondBarBottom > 0) {
+    // Imposta l'altezza delle altre barre in modo che siano uguali all'altezza della seconda barra
+    bars3.forEach((bar) => {
+      bar.style.height = `${secondBarHeight}px`;
+    });
+  }
+});
 
   // Osserva l'elemento target
   observer.observe(target);
